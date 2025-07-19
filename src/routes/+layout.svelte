2031b0da55faaf521bf2f53d62baa98e04c92c1a
Aiden Redmond <adnrdmnd@gmail.com>
2025-07-19 19:09:33 -0400

    pre-supabase

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,16 @@
     import "../app.css"
 </script>
 
-<div class="bg-sage flex flex-col flex-1 min-h-screen">
-    <nav class="flex items-center justify-center gap-12 pt-6">
-        <a href="/">dashboard</a>
-        <a href="/transactions">transactions</a>
-        <a href="/settings">settings</a>
+<div class="bg-sage flex flex-col flex-1 min-h-screen text-deep-green">
+    <nav class="flex items-center justify-between gap-12 pt-6 px-6">
+        <a href="/" class="text-2xl font-bold">untitled budget app</a>
+        <div class="flex gap-6">
+            <a href="/">dashboard</a>
+            <a href="/transactions">transactions</a>
+            <a href="/settings">settings</a>
+        </div>
     </nav>
-    <div class="pt-12 text-center">
+    <div class="flex flex-col pt-12 items-center justify-center">
         {@render children()}
     </div>
 </div>
--- src/routes/transactions/+page.svelte
+++ src/routes/transactions/+page.svelte
@@ -1,1 +1,22 @@
-<h1>hey there this is the transactions page!</h1>
+<table class="table-auto">
+    <thead class="bg-deep-green/33">
+        <tr class="border text-center">
+            <th scope="col" class="border-r px-6 py-1 text-xl">Date</th>
+            <th scope="col" class="border-r px-6 py-1 text-xl">Description</th>
+            <th scope="col" class="border-r px-6 py-1 text-xl">Group</th>
+            <th scope="col" class="border-r px-6 py-1 text-xl">Category</th>
+            <th scope="col" class="border-r px-6 py-1 text-xl">Amount</th>
+            <th scope="col" class="border-r px-6 py-1 text-xl">Card</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr class="border text-center">
+            <th class="border-r px-6 py-1">7/19/25</th>
+            <td class="border-r px-6 py-1">i bought chipotle for dinner</td>
+            <td class="border-r px-6 py-1">food</td>
+            <td class="border-r px-6 py-1">eating out</td>
+            <td class="border-r px-6 py-1">$9.96</td>
+            <td class="border-r px-6 py-1">credit</td>
+        </tr>
+    </tbody>
+</table>
